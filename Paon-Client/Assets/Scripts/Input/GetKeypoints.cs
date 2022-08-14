f3bb508cb6359d02c90b4594b7fd550b843f4c14
LITl-l <fafeeyu@gmail.com>
2022-08-14 12:21:36 +0900

    とりま動くとこまで

--- Paon-Client/Assets/Scripts/Input/GetKeypoints.cs
+++ Paon-Client/Assets/Scripts/Input/GetKeypoints.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using OpenPose;
-using OpenPose.Example;
+// using OpenPose;
+// using OpenPose.Example;
 using UnityEngine;
 
 namespace Paon.NInput
 {
     public class GetKeypoints : MonoBehaviour
     {
+        // private OPDatum datum;
+        // public OpenPoseUserScript op;
+        [SerializeField]
+        private GameObject Estimatior;
 
-        private OPDatum datum;
+        private PoseEstimate _PoseEstimate;
 
-        public OpenPoseUserScript op;
+        private Utils.Keypoint[] poseKeypoints;
 
         public enum KeyPointType : byte
         {
@@ -41,58 +45,66 @@ namespace Paon.NInput
 
         public Keypoint[] left = new Keypoint[17];
 
-        public void LoadDatum(int length, KeyPointType type)
-        {
-            int index = 0;
-            try
-            {
-
-
-                for (int i = 0; i < length * 3; i += 3)
-                {
-                    if (type == KeyPointType.pk)
-                    {
-                        pose[index] =
-                            new Keypoint(datum.poseKeypoints[i],
-                                datum.poseKeypoints[i + 1],
-                                datum.poseKeypoints[i + 2]);
-                        index++;
-                    }
-                    else if (type == KeyPointType.lhk)
-                    {
-                        left[index] =
-                            new Keypoint(datum.handKeypoints[0][i],
-                                datum.handKeypoints[0][i + 1],
-                                datum.handKeypoints[0][i + 2]);
-                        index++;
-                    }
-                    else if (type == KeyPointType.rhk)
-                    {
-                        right[index] =
-                            new Keypoint(datum.handKeypoints[1][i],
-                                datum.handKeypoints[1][i + 1],
-                                datum.handKeypoints[1][i + 2]);
-                        index++;
-                    }
-                }
-            }
-            catch (System.NullReferenceException e) {
-                
-            }
-        }
-
+        // public void LoadDatum(int length, KeyPointType type)
+        // {
+        //     int index = 0;
+        //     try
+        //     {
+        //         for (int i = 0; i < length * 3; i += 3)
+        //         {
+        //             if (type == KeyPointType.pk)
+        //             {
+        //                 pose[index] =
+        //                     new Keypoint(datum.poseKeypoints[i],
+        //                         datum.poseKeypoints[i + 1],
+        //                         datum.poseKeypoints[i + 2]);
+        //                 index++;
+        //             }
+        //             else if (type == KeyPointType.lhk)
+        //             {
+        //                 left[index] =
+        //                     new Keypoint(datum.handKeypoints[0][i],
+        //                         datum.handKeypoints[0][i + 1],
+        //                         datum.handKeypoints[0][i + 2]);
+        //                 index++;
+        //             }
+        //             else if (type == KeyPointType.rhk)
+        //             {
+        //                 right[index] =
+        //                     new Keypoint(datum.handKeypoints[1][i],
+        //                         datum.handKeypoints[1][i + 1],
+        //                         datum.handKeypoints[1][i + 2]);
+        //                 index++;
+        //             }
+        //         }
+        //     }
+        //     catch (System.NullReferenceException e)
+        //     {
+        //     }
+        // }
         void Start()
         {
-            op = GameObject.Find("OpenPose").GetComponent<OpenPoseUserScript>();
+            // op = GameObject.Find("OpenPose").GetComponent<OpenPoseUserScript>();
+            _PoseEstimate = Estimatior.GetComponent<PoseEstimate>();
         }
 
         void LateUpdate()
         {
-            datum = op.datum;
-            LoadDatum(25, KeyPointType.pk);
-            LoadDatum(17, KeyPointType.rhk);
-            LoadDatum(17, KeyPointType.lhk);
+            // datum = op.datum;
+            // LoadDatum(25, KeyPointType.pk);
+            // LoadDatum(17, KeyPointType.rhk);
+            // LoadDatum(17, KeyPointType.lhk);
             //Debug.Log(datum.poseKeypoints[0]);
+            poseKeypoints = _PoseEstimate.GetKeypoints();
+            int cnt = 0;
+            foreach (Utils.Keypoint key in poseKeypoints)
+            {
+                pose[cnt] =
+                    new Keypoint(key.position.x, key.position.y, key.score);
+
+                // Debug.Log("pose[" + cnt + "]: " + pose[cnt].coords);
+                cnt++;
+            }
         }
     }
 }
