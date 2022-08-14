1e20b4ad5d07c258606f72cdb0b3dc19e345e32c
Harumaso500 <haru006@hotmail.com>
2022-08-15 00:53:18 +0900

    tempのServerとSharedを新しくしました。

--- Paon-Client/Assets/Scripts/Input/GetKeypoints.cs
+++ Paon-Client/Assets/Scripts/Input/GetKeypoints.cs
@@ -1,28 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
-using MediaPipe.HandPose;
-// using OpenPose;
-// using OpenPose.Example;
+using OpenPose;
+using OpenPose.Example;
 using UnityEngine;
 
 namespace Paon.NInput
 {
     public class GetKeypoints : MonoBehaviour
     {
-        // private OPDatum datum;
-        // public OpenPoseUserScript op;
-        [SerializeField]
-        private GameObject PoseEstimatior;
 
-        [SerializeField]
-        private GameObject HandEstimatior;
+        private OPDatum datum;
 
-        private PoseEstimate _PoseEstimate;
-
-        private Utils.Keypoint[] poseKeypoints;
-
-        private HandVisualizer _handVisualizer;
+        public OpenPoseUserScript op;
 
         public enum KeyPointType : byte
         {
@@ -33,94 +23,76 @@ namespace Paon.NInput
 
         public class Keypoint
         {
-            public Vector3 coords;
+            public Vector2 coords;
 
             public float s;
 
-            public Keypoint(float x, float y, float z, float s)
+            public Keypoint(float x, float y, float s)
             {
                 this.coords.x = x;
                 this.coords.y = y;
-                this.coords.z = z;
                 this.s = s;
             }
         }
 
         public Keypoint[] pose = new Keypoint[25];
 
-        public Keypoint[] right = new Keypoint[21];
+        public Keypoint[] right = new Keypoint[17];
 
-        public Keypoint[] left = new Keypoint[21];
+        public Keypoint[] left = new Keypoint[17];
 
-        // public void LoadDatum(int length, KeyPointType type)
-        // {
-        //     int index = 0;
-        //     try
-        //     {
-        //         for (int i = 0; i < length * 3; i += 3)
-        //         {
-        //             if (type == KeyPointType.pk)
-        //             {
-        //                 pose[index] =
-        //                     new Keypoint(datum.poseKeypoints[i],
-        //                         datum.poseKeypoints[i + 1],
-        //                         datum.poseKeypoints[i + 2]);
-        //                 index++;
-        //             }
-        //             else if (type == KeyPointType.lhk)
-        //             {
-        //                 left[index] =
-        //                     new Keypoint(datum.handKeypoints[0][i],
-        //                         datum.handKeypoints[0][i + 1],
-        //                         datum.handKeypoints[0][i + 2]);
-        //                 index++;
-        //             }
-        //             else if (type == KeyPointType.rhk)
-        //             {
-        //                 right[index] =
-        //                     new Keypoint(datum.handKeypoints[1][i],
-        //                         datum.handKeypoints[1][i + 1],
-        //                         datum.handKeypoints[1][i + 2]);
-        //                 index++;
-        //             }
-        //         }
-        //     }
-        //     catch (System.NullReferenceException e)
-        //     {
-        //     }
-        // }
+        public void LoadDatum(int length, KeyPointType type)
+        {
+            int index = 0;
+            try
+            {
+
+
+                for (int i = 0; i < length * 3; i += 3)
+                {
+                    if (type == KeyPointType.pk)
+                    {
+                        pose[index] =
+                            new Keypoint(datum.poseKeypoints[i],
+                                datum.poseKeypoints[i + 1],
+                                datum.poseKeypoints[i + 2]);
+                        index++;
+                    }
+                    else if (type == KeyPointType.lhk)
+                    {
+                        left[index] =
+                            new Keypoint(datum.handKeypoints[0][i],
+                                datum.handKeypoints[0][i + 1],
+                                datum.handKeypoints[0][i + 2]);
+                        index++;
+                    }
+                    else if (type == KeyPointType.rhk)
+                    {
+                        right[index] =
+                            new Keypoint(datum.handKeypoints[1][i],
+                                datum.handKeypoints[1][i + 1],
+                                datum.handKeypoints[1][i + 2]);
+                        index++;
+                    }
+                }
+            }
+            catch (System.NullReferenceException e) {
+                
+            }
+        }
+
         void Start()
         {
-            // op = GameObject.Find("OpenPose").GetComponent<OpenPoseUserScript>();
-            _PoseEstimate = PoseEstimatior.GetComponent<PoseEstimate>();
-            _handVisualizer = HandEstimatior.GetComponent<HandVisualizer>();
+            op = GameObject.Find("OpenPose").GetComponent<OpenPoseUserScript>();
         }
 
         void LateUpdate()
         {
-            // datum = op.datum;
-            // LoadDatum(25, KeyPointType.pk);
-            // LoadDatum(17, KeyPointType.rhk);
-            // LoadDatum(17, KeyPointType.lhk);
+            datum = op.datum;
+            LoadDatum(25, KeyPointType.pk);
+            LoadDatum(17, KeyPointType.rhk);
+            LoadDatum(17, KeyPointType.lhk);
             //Debug.Log(datum.poseKeypoints[0]);
-            poseKeypoints = _PoseEstimate.GetKeypoints();
-            int cnt = 0;
-            foreach (Utils.Keypoint key in poseKeypoints)
-            {
-                pose[cnt] =
-                    new Keypoint(key.position.x, key.position.y, 0, key.score);
-
-                // Debug.Log("pose[" + cnt + "]: " + pose[cnt].coords);
-                cnt++;
-            }
-
-            Vector3[] pos = _handVisualizer.GetKey();
-            Debug.Log(pos[20]);
-
-            for (int i = 0; i < left.Length; i++)
-            {
-                left[i] = new Keypoint(pos[i].x, pos[i].y, pos[i].z, 0);
-            }
         }
     }
 }
--- Paon-Client/Assets/Scripts/Player/PlayerMove.cs
+++ Paon-Client/Assets/Scripts/Player/PlayerMove.cs
@@ -2,38 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Paon.NInput;
-public class PlayerMove : MonoBehaviour
+using Grpc.Core;
+using MagicOnion.Client;
+using Paon.NNetWork.Shared.Services;
+using Paon;
+using Paon.NNetwork.Shared.MessagePackObjects;
+
+namespace Paon.NPlayer
 {
-    MoveInputProvider inputProvider;
-    public GameObject player;
-    public GameObject es;
-    void Start()
+    public class PlayerMove : MonoBehaviour
     {
-        inputProvider = es.GetComponent<MoveInputProvider>();
-        player = this.gameObject;
-    }
-    void Update()
-    {
-        if (inputProvider.GetInput() == "space")
+        MoveInputProvider inputProvider;
+        public GameObject player;
+        public GameObject es;
+        private GamingHubClient _hub;
+        private Channel _channel;
+        private IMyFirstService _service;
+
+        void Awake()
         {
-            player.GetComponent<Rigidbody>().AddForce(Vector3.up * 0.25f, ForceMode.Impulse);
+            _channel = new Channel("192.168.11.2", 5032, ChannelCredentials.Insecure);
+            _service = MagicOnionClient.Create<IMyFirstService>(_channel);
         }
-        else if (inputProvider.GetInput() == "up")
+
+        async void Start()
         {
-            player.transform.Translate(Vector3.forward * 0.1f);
+            var x = Random.Range(0, 1000);
+            var y = Random.Range(0, 1000);
+            var result = await _service.SumAsync(x, y);
+            Debug.Log($"Result: {result}");
+
+            inputProvider = es.GetComponent<MoveInputProvider>();
+            player = this.gameObject;
+            var id = Random.Range(0, 10000);
+            _hub = new GamingHubClient();
+            await _hub.ConnectAsync(_channel, "Room", $"Player-{id}");
         }
-        else if (inputProvider.GetInput() == "down")
+
+        async void Update()
         {
-            player.transform.Translate(Vector3.back * 0.03f);
+            if (inputProvider.GetInput() == "space")
+            {
+                player.GetComponent<Rigidbody>().AddForce(Vector3.up * 0.25f, ForceMode.Impulse);
+            }
+            else if (inputProvider.GetInput() == "up")
+            {
+                player.transform.Translate(Vector3.forward * 0.1f);
+                await _hub.MoveAsync(player.transform.position, new Vector3());
+            }
+            else if (inputProvider.GetInput() == "down")
+            {
+                player.transform.Translate(Vector3.back * 0.03f);
+                await _hub.MoveAsync(player.transform.position, new Vector3());
+            }
+            else if (inputProvider.GetInput() == "left")
+            {
+                player.transform.Rotate(0, -0.15f, 0);
+                await _hub.MoveAsync(new Vector3(), player.transform.eulerAngles);
+            }
+            else if (inputProvider.GetInput() == "right")
+            {
+                player.transform.Rotate(0, 0.15f, 0);
+                await _hub.MoveAsync(new Vector3(), player.transform.eulerAngles);
+            }
+            //Debug.Log(inputProvider.GetInput());
         }
-        else if (inputProvider.GetInput() == "left")
+
+        async void OnDestroy()
         {
-            player.transform.Rotate(0, -0.15f, 0);
+            if (_hub != null)
+            {
+                await _hub.DisposeAsync();
+            }
+            if (_channel != null)
+            {
+                await _channel.ShutdownAsync();
+            }
         }
-        else if (inputProvider.GetInput() == "right")
-        {
-            player.transform.Rotate(0, 0.15f, 0);
-        }
-        //Debug.Log(inputProvider.GetInput());
+
     }
 }
