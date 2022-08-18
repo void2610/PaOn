cf91cb4a9f4db3535804929558f6cf56cc469326
LITl-l <fafeeyu@gmail.com>
2022-08-19 00:46:44 +0900

    update HandEstimation.cs ...etc

--- Paon-Client/Assets/Brracuda/Scripts/HandEstimation.cs
+++ Paon-Client/Assets/Brracuda/Scripts/HandEstimation.cs
@@ -93,6 +93,80 @@ namespace MediaPipe.HandPose
             var scale =
                 new Vector2(Mathf.Max((float) input.height / input.width, 1),
                     Mathf.Max(1, (float) input.width / input.height));
+
+            commonCS.SetVector("_spadScale", scale);
+            commonCS.SetInt("_spadWidth", input.width);
+            commonCS.SetTexture(0, "_input", input);
+            commonCS.SetTexture(0, "_render", _render);
+            commonCS.Dispatch(0, imageWidth / 8, imageWidth / 8, 1);
+
+            HandProcess (input, _render, scale);
+        }
+
+        void HandProcess(Texture input, Texture _render, Vector2 spadScale)
+        {
+            palmDetector.ProcessImage (_render);
+
+            int[] countReadCache = new int[1];
+            palmDetector.CountBuffer.GetData(countReadCache, 0, 0, 1);
+            var handDetectionCount = countReadCache[0];
+            handDetectionCount = (int) Mathf.Min(handDetectionCount, 2);
+
+            if (handDetectionCount > 0)
+            {
+                handCS.SetInt("_detectionCount", handDetectionCount);
+                handCS.SetFloat("_regionDetectDt", Time.deltaTime);
+                handCS
+                    .SetBuffer(0,
+                    "_palmDetections",
+                    palmDetector.DetectionBuffer);
+                handCS
+                    .SetBuffer(0, "_handsRegionFromPalm", handsRegionFromPalm);
+                handCS.Dispatch(0, 1, 1, 1);
+            }
+
+            handCS.SetVector("_spadScale", spadScale);
+            handCS.SetInt("_isMirrored", 1);
+            for (int i = 0; i < handDetectionCount; i++)
+            {
+                handCS.SetInt("_handRegionIndex", i);
+
+                handCS.SetInt("_handCropSize", handCropImageSize);
+                handCS.SetTexture(2, "_handCropInput", inputTexture);
+                handCS.SetBuffer(2, "_handCropRegion", handsRegionFromPalm);
+                handCS.SetBuffer(2, "_handCropOutput", handCropBuffer);
+                handCS
+                    .Dispatch(2,
+                    handCropImageSize / 8,
+                    handCropImageSize / 8,
+                    1);
+
+                handLandmarkDetector.ProcessImage (handCropBuffer);
+
+                var ScoreCache = new Vector4[1];
+                handLandmarkDetector.OutputBuffer.GetData(scoreCache, 0, 0, 1);
+                float score = scoreCache[0].x;
+                float handedness = scoreCache[0].y;
+                bool isRight = handedness > 0.5f;
+
+                handCS.SetFloat("_handPostDt", Time.deltaTime);
+                handCS
+                    .SetBuffer(3,
+                    "_handPostInput",
+                    handLandmarkDetector.OutputBuffer);
+                handCS.SetBuffer(3, "_handPostRegion", handsRegionFromPalm);
+                handCS
+                    .SetBuffer(3,
+                    "_handPostOutput",
+                    isRight ? rightHandVertexBuffer : leftHandVertexBuffer);
+                handCS
+                    .SetBuffer(3,
+                    "_handPostDeltaOutput",
+                    isRight
+                        ? deltaRightHandVertexBuffer
+                        : deltaLeftHandVertexBuffer);
+                handCS.Dispatch(3, 1, 1, 1);
+            }
         }
 
         // Update is called once per frame
