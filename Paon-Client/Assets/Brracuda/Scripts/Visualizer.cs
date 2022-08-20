b23ef98ab2fc88d0f58d0c84a13f5ee3dff9e3cb
LITl-l <fafeeyu@gmail.com>
2022-08-20 15:48:55 +0900

    fix: visualize.cs

--- Paon-Client/Assets/Brracuda/Scripts/Visualizer.cs
+++ Paon-Client/Assets/Brracuda/Scripts/Visualizer.cs
@@ -7,53 +7,53 @@ using UnityEngine.UI;
 
 public class Visualizer : MonoBehaviour
 {
-//     [SerializeField]
-//     WebCamInput _webcam;
+    [SerializeField]
+    WebCamInput _webcam;
 
-//     [SerializeField]
-//     RawImage _image = null;
+    [SerializeField]
+    RawImage _image = null;
 
-//     [SerializeField]
-//     Shader _shader = null;
+    [SerializeField]
+    Shader _shader = null;
 
-//     [SerializeField, Range(0, 1)]
-//     float handScoreThreshold = 0.5f;
+    [SerializeField, Range(0, 1)]
+    float handScoreThreshold = 0.5f;
 
-//     [SerializeField]
-//     Resource resource = null;
+    [SerializeField]
+    Resource resource = null;
 
-//     HandPose _handPose;
+    HandPose _handPose;
 
-//     Material _material;
+    Material _material;
 
-//     // Start is called before the first frame update
-//     void Start()
-//     {
-//         _handPose = new HandPose(resource);
-//         _material = new Material(_shader);
-//     }
+    // Start is called before the first frame update
+    void Start()
+    {
+        _handPose = new HandPose(resource);
+        _material = new Material(_shader);
+    }
 
-//     void OnDestroy()
-//     {
-//         Destroy (_material);
-//     }
+    void OnDestroy()
+    {
+        Destroy (_material);
+    }
 
-//     void LateUpdate()
-//     {
-//         _image.texture = _webcam.inputTexture;
-//         _handPose.ProcessImage(_webcam.inputImageTexture);
-//     }
+    void LateUpdate()
+    {
+        _image.texture = _webcam.inputTexture;
+        _handPose.ProcessImage(_webcam.inputImageTexture);
+    }
 
-// void HandRender(bool isRight){
-// 	var w = _image.rectTransform..rect.width;
-// 	var h = _image.rectTransform..rect.height;
-// 	_material.setVector("_uiScale", new Vector2(w, h));
-// 	_material.setVector("_pointColor", isRight ? Color.cyan : Color.yellow);
-// 	_material.setFloat("_handScoreThreshold", handScoreThreshold);
-// }
+void HandRender(bool isRight){
+	var w = _image.rectTransform..rect.width;
+	var h = _image.rectTransform..rect.height;
+	_material.SetVector("_uiScale", new Vector2(w, h));
+	_material.SetVector("_pointColor", isRight ? Color.cyan : Color.yellow);
+	_material.SetFloat("_handScoreThreshold", handScoreThreshold);
+}
 
-//     void OnDestroy()
-//     {
-//         HandPose.Dispose();
-//     }
+    void OnDestroy()
+    {
+        HandPose.Dispose();
+    }
 }
