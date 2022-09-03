41c733346e6b03498745ee5a764c6fc16403cec1
void2610 <izumisan2610@gmail.com>
2022-09-03 15:15:12 +0900

    最近の作品を表示

---
+++ Paon-Client/Assets/Scripts/NaturePlay/LoadPictureScript.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class LoadPictureScript : MonoBehaviour
+{
+    GameObject[] pictures = new GameObject[5];
+
+    string path;
+
+    void LoadPicture()
+    {
+        //ファイル名
+        string[] files =
+            Directory.GetFiles(path, "*.png", SearchOption.AllDirectories);
+        Array.Sort (files);
+        int n = 0;
+        if (files.Length < 5)
+        {
+            n = files.Length;
+        }
+        else
+        {
+            n = 5;
+        }
+        for (int i = 0; i < n; i++)
+        {
+            if (files[i] != null)
+            {
+                //Debug.Log(files[i]);
+                byte[] bytes = File.ReadAllBytes(files[i]);
+                Texture2D texture = new Texture2D(2, 2);
+                texture.LoadImage (bytes);
+                pictures[i].GetComponent<Renderer>().material.mainTexture =
+                    texture;
+            }
+        }
+    }
+
+    void Start()
+    {
+        path = Application.dataPath + "/Resources/NaturePlay";
+        pictures[0] = GameObject.Find("Picture1");
+        pictures[1] = GameObject.Find("Picture2");
+        pictures[2] = GameObject.Find("Picture3");
+        pictures[3] = GameObject.Find("Picture4");
+        pictures[4] = GameObject.Find("Picture5");
+        LoadPicture();
+    }
+
+    void Update()
+    {
+    }
+}
