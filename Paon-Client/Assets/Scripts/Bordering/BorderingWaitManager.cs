9b4e1025194d397ea65acfc1f6452ea8b5024c3f
void2610 <izumisan2610@gmail.com>
2022-09-08 02:22:04 +0900

    ボルダリング待ちエリア

---
+++ Paon-Client/Assets/Scripts/Bordering/BorderingWaitManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paon.NBordering
+{
+    public class BorderingWaitManager : MonoBehaviour
+    {
+        public int NowPeople = 0;
+
+        public int MaxPeople = 3;
+
+        public GameObject[] WaitAreas = new GameObject[3];
+
+        void Start()
+        {
+            WaitAreas[0] = GameObject.Find("WaitArea1");
+            WaitAreas[1] = GameObject.Find("WaitArea2");
+            WaitAreas[2] = GameObject.Find("WaitArea3");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (NowPeople < MaxPeople)
+            {
+                WaitAreas[0].GetComponent<WaitAreaScript>().TeleportPlayer();
+            }
+            if (WaitAreas[1].GetComponent<WaitAreaScript>().ReadyPlayer != null)
+            {
+                WaitAreas[1].GetComponent<WaitAreaScript>().TeleportPlayer();
+            }
+            if (WaitAreas[2].GetComponent<WaitAreaScript>().ReadyPlayer != null)
+            {
+                WaitAreas[2].GetComponent<WaitAreaScript>().TeleportPlayer();
+            }
+        }
+    }
+}
---
+++ Paon-Client/Assets/Scripts/Bordering/WaitAreaScript.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paon.NBordering
+{
+    public class WaitAreaScript : MonoBehaviour
+    {
+        public GameObject ReadyPlayer = null;
+
+        public GameObject NextPosition;
+
+        public void TeleportPlayer()
+        {
+            if (ReadyPlayer != null)
+            {
+                ReadyPlayer.transform.position =
+                    NextPosition.transform.position;
+                ReadyPlayer = null;
+            }
+        }
+
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            TeleportPlayer();
+        }
+
+        void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (ReadyPlayer == null)
+                {
+                    ReadyPlayer = other.gameObject;
+                }
+            }
+            else
+            {
+                ReadyPlayer = null;
+            }
+        }
+    }
+}
