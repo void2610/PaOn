55a5394262d0c7f3b9e23a0ff5dbfd26f0cfeab2
void2610 <izumisan2610@gmail.com>
2022-09-08 23:27:09 +0900

    タイマー

---
+++ Paon-Client/Assets/Scripts/UI/TimerScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    float time = 0.0f;
+
+    public bool counting = false;
+
+    void CountStart()
+    {
+        time = 0.0f;
+        counting = true;
+    }
+
+    void CountStop()
+    {
+        counting = false;
+    }
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (counting)
+        {
+            time += Time.deltaTime;
+        }
+        if (time > 0)
+        {
+            this.gameObject.GetComponent<Text>().text = time.ToString("F2");
+        }
+        else
+        {
+            this.gameObject.GetComponent<Text>().text = "";
+        }
+    }
+}
