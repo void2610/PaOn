d46389b0f30a817f48e4c360d9978ad07155eec3
void2610 <izumisan2610@gmail.com>
2022-09-28 19:12:28 +0900

    効果音追加とリソース内整理

---
+++ Paon-Client/Assets/Scripts/Audio/ConflictSEScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Paon.NAudio
+{
+    public class ConflictSEScript : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        public AudioClip SE;
+
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            //Debug.Log(this.GetComponent<Rigidbody>().velocity.magnitude);
+        }
+
+        void OnCollisionEnter(Collision collision)
+        {
+            Debug.Log("Hit");
+            if (this.GetComponent<Rigidbody>().velocity.magnitude > 0.1f)
+            {
+                if (this.GetComponent<AudioSource>())
+                {
+                    GetComponent<AudioSource>().PlayOneShot(SE);
+                }
+            }
+        }
+    }
+}
