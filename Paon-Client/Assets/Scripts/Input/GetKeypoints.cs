981a98e7b7705268c415120b56ea90c1ee9f994a
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-01 19:26:12 +0900

    fix: close

--- Paon-Client/Assets/Scripts/Input/GetKeypoints.cs
+++ Paon-Client/Assets/Scripts/Input/GetKeypoints.cs
@@ -108,29 +108,18 @@ namespace Paon.NInput
 			//             0);
 			// }
 
-			leftIsClosed = leftCloseOrOpen(leftTemp);
-			rightIsClosed = rightCloseOrOpen(rightTemp);
+			leftIsClosed = CloseOrOpen(leftTemp);
+			rightIsClosed = CloseOrOpen(rightTemp);
 			// Debug.Log("Left: " + leftIsClosed);
 			// Debug.Log("Right: " + rightIsClosed);
 		}
 
-		private int leftCloseOrOpen(Vector3[] finger)
+		private int CloseOrOpen(Vector3[] finger)
 		{
 			float distance = Vector3.Distance(finger[0], finger[12]);
 			// Debug.Log("Distance: " + distance);
 			if (distance < holdThreshold)
 				return 1;
-			else return 0;
-		}
-		private int rightCloseOrOpen(Vector3[] finger)
-		{
-			float distance = Vector3.Distance(finger[0], finger[12]);
-			Debug.Log("rightDistance: " + distance);
-			if (distance < holdThreshold)
-			{
-				Debug.Log("right is closed");
-				return 1;
-			}
 			else return 0;
 		}
 	}
