4830b35b3deaf16cf7e9d6d719ecac630b105f5b
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-01 17:58:06 +0900

    fix: move input provide (crouch is public)

--- Paon-Client/Assets/Scripts/UI/Calibration.cs
+++ Paon-Client/Assets/Scripts/UI/Calibration.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Paon.NInput;
@@ -5,27 +6,65 @@ using UnityEngine;
 
 public class Calibration : MonoBehaviour
 {
-	enum phase
+	public enum Phase
 	{
-		Positioning,
+		PoseEstimation,
 		HandEstimation,
+		Positioning,
 	};
+
+	public Phase state;
+
+	public float minConfidence = 0.7f;
 
 	[SerializeField]
 	private Visualizer _visualizer;
 
+	private Vector3[] left, right;
+
 	[SerializeField]
 	private GetKeypoints gk;
+
+	private GetKeypoints.Keypoint[] pose;
 
 	// Start is called before the first frame update
 	void Start()
 	{
-
+		pose = gk.pose;
+		state = Phase.PoseEstimation;
 	}
 
 	// Update is called once per frame
 	void Update()
 	{
+		pose = gk.pose;
+		left = _visualizer.GetLeftVert();
+		right = _visualizer.GetRightVert();
+
+		switch (state)
+		{
+			case Phase.PoseEstimation:
+				Debug.Log("Phase1");
+				if (pose[0].score > minConfidence && pose[15].score > minConfidence && pose[16].score > minConfidence)
+				{
+					state = Phase.Positioning;
+				}
+				return;
+			case Phase.HandEstimation:
+				Debug.Log("Phase2");
+				return;
+			case Phase.Positioning:
+				Debug.Log("Phase3");
+				return;
+			default:
+				Debug.Log("Calibration end");
+				return;
+		}
+
+		void InitPose()
+		{
+
+		}
 
 	}
 }
