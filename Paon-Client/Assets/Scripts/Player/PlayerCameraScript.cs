92bbeee42c78dcc8e6cf1c32442de17f09be9e8d
void2610 <izumisan2610@gmail.com>
2022-10-01 19:20:21 +0900

    しゃがみのカメラ移動(要調整)

--- Paon-Client/Assets/Scripts/Player/PlayerCameraScript.cs
+++ Paon-Client/Assets/Scripts/Player/PlayerCameraScript.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using Paon.NBordering;
+using Paon.NInput;
 using UnityEngine;
 
 namespace Paon.NPlayer
@@ -15,8 +16,13 @@ namespace Paon.NPlayer
 
         bool tmp = false;
 
+        int c = 0;
+
+        GameObject mip;
+
         void Start()
         {
+            mip = GameObject.Find("MoveInputProvider");
             if (GameObject.Find("BorderingGoal") != null)
             {
                 goal =
@@ -36,6 +42,11 @@ namespace Paon.NPlayer
         // Update is called once per frame
         void Update()
         {
+            c = mip.GetComponent<MoveInputProvider>().crouch;
+            float y = this.gameObject.transform.position.y;
+            Vector3 rot = this.gameObject.transform.eulerAngles;
+            Vector3 lp = new Vector3(0, 2.3f, 0);
+
             if (goal != null)
             {
                 if (!tmp && goal.goaling)
@@ -43,74 +54,102 @@ namespace Paon.NPlayer
                     stat = 1;
                 }
             }
-            if (stat == 1 && start.starting)
-            {
-                stat = 2;
-            }
-            float y = this.gameObject.transform.position.y;
-            Vector3 rot;
-            if (stat == 1)
-            {
-                rot =
-                    new Vector3(25,
-                        this.gameObject.transform.eulerAngles.y,
-                        this.gameObject.transform.eulerAngles.z);
-            }
-            else if (stat == 2 && y < 0.5f)
-            {
-                rot =
-                    new Vector3(0,
-                        this.gameObject.transform.eulerAngles.y,
-                        this.gameObject.transform.eulerAngles.z);
-            }
-            else if (y > 0.5f && stat == 0)
-            {
-                if (y < 1.5f)
-                {
-                    rot =
-                        new Vector3(-15 * y,
-                            this.gameObject.transform.eulerAngles.y,
-                            this.gameObject.transform.eulerAngles.z);
-                }
-                else
-                {
-                    rot =
-                        new Vector3(-22.5f,
-                            this.gameObject.transform.eulerAngles.y,
-                            this.gameObject.transform.eulerAngles.z);
-                }
-            }
-            else if (y > 0.5f && stat == 2)
-            {
-                Debug.Log("stat2");
-                if (y < 1.5f)
-                {
-                    rot =
-                        new Vector3(-15 * y,
-                            this.gameObject.transform.eulerAngles.y,
-                            this.gameObject.transform.eulerAngles.z);
-                }
-                else
-                {
-                    rot =
-                        new Vector3(-22.5f,
-                            this.gameObject.transform.eulerAngles.y,
-                            this.gameObject.transform.eulerAngles.z);
-                }
-            }
-            else
-            {
-                rot =
-                    new Vector3(0,
-                        this.gameObject.transform.eulerAngles.y,
-                        this.gameObject.transform.eulerAngles.z);
-            }
 
-            this.gameObject.transform.eulerAngles = rot;
-            if (goal != null)
+            if (
+                PlayerPrefs.GetString("Room", "none") == "Nature1" ||
+                PlayerPrefs.GetString("Room", "none") == "Nature2"
+            )
             {
-                tmp = goal.goaling;
+                Debug.Log (c);
+                if (c == 0)
+                {
+                    rot =
+                        new Vector3(25,
+                            this.gameObject.transform.eulerAngles.y,
+                            this.gameObject.transform.eulerAngles.z);
+                    lp = new Vector3(0, 1.3f, 0);
+                }
+                else
+                {
+                    rot =
+                        new Vector3(0,
+                            this.gameObject.transform.eulerAngles.y,
+                            this.gameObject.transform.eulerAngles.z);
+                }
             }
+            else if (
+                PlayerPrefs.GetString("Room", "none") == "Bordering1" ||
+                PlayerPrefs.GetString("Room", "none") == "Bordering2"
+            )
+            {
+                if (stat == 1 && start.starting)
+                {
+                    stat = 2;
+                }
+
+                if (stat == 1)
+                {
+                    rot =
+                        new Vector3(25,
+                            this.gameObject.transform.eulerAngles.y,
+                            this.gameObject.transform.eulerAngles.z);
+                }
+                else if (stat == 2 && y < 0.5f)
+                {
+                    rot =
+                        new Vector3(0,
+                            this.gameObject.transform.eulerAngles.y,
+                            this.gameObject.transform.eulerAngles.z);
+                }
+                else if (y > 0.5f && stat == 0)
+                {
+                    if (y < 1.5f)
+                    {
+                        rot =
+                            new Vector3(-15 * y,
+                                this.gameObject.transform.eulerAngles.y,
+                                this.gameObject.transform.eulerAngles.z);
+                    }
+                    else
+                    {
+                        rot =
+                            new Vector3(-22.5f,
+                                this.gameObject.transform.eulerAngles.y,
+                                this.gameObject.transform.eulerAngles.z);
+                    }
+                }
+                else if (y > 0.5f && stat == 2)
+                {
+                    Debug.Log("stat2");
+                    if (y < 1.5f)
+                    {
+                        rot =
+                            new Vector3(-15 * y,
+                                this.gameObject.transform.eulerAngles.y,
+                                this.gameObject.transform.eulerAngles.z);
+                    }
+                    else
+                    {
+                        rot =
+                            new Vector3(-22.5f,
+                                this.gameObject.transform.eulerAngles.y,
+                                this.gameObject.transform.eulerAngles.z);
+                    }
+                }
+                else
+                {
+                    rot =
+                        new Vector3(0,
+                            this.gameObject.transform.eulerAngles.y,
+                            this.gameObject.transform.eulerAngles.z);
+                }
+
+                if (goal != null)
+                {
+                    tmp = goal.goaling;
+                }
+            }
+            this.gameObject.transform.eulerAngles = rot;
         }
     }
 }
