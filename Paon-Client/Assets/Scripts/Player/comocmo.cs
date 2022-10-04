994346a451a91a1a3791c0312667c2a221a2cb94
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-04 19:51:49 +0900

    tempsuh

--- Paon-Client/Assets/Scripts/Player/comocmo.cs
+++ Paon-Client/Assets/Scripts/Player/comocmo.cs
@@ -34,7 +34,8 @@ public class comocmo : MonoBehaviour
 		left = gk.leftWrist;
 		right = gk.rightWrist;
 		delta = Mathf.Abs(left.coords.y - right.coords.y);
-		if (delta > threshhold && !check) StartCoroutine(nameof(CommoRose));
+		if (delta > threshhold && !check) check = true;
+		if (check) StartCoroutine(nameof(CommoRose));
 
 	}
 
