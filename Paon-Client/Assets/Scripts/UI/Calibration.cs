4710a72cb55d60d857115b86148187cfad711a0f
LITl-l <fafeeyu@gmail.com>
2022-10-05 23:09:21 +0900

    update: message shown

--- Paon-Client/Assets/Scripts/UI/Calibration.cs
+++ Paon-Client/Assets/Scripts/UI/Calibration.cs
@@ -4,167 +4,170 @@ using System.Collections.Generic;
 using System.Linq;
 using Paon.NInput;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class Calibration : MonoBehaviour
 {
-	public enum Phase
-	{
-		PoseEstimation,
-		HandEstimation,
-		Positioning,
-		End,
-	};
+    public enum Phase
+    {
+        PoseEstimation,
+        HandEstimation,
+        Positioning,
+        End,
+    };
 
-	public Phase state;
+    public Phase state;
 
-	public float minConfidence = 0.7f;
+    public float minConfidence = 0.7f;
 
-	[SerializeField]
-	private Visualizer _visualizer;
+    public bool end = false;
 
-	[SerializeField]
-	GetKeypoints gk;
+    [SerializeField]
+    private Visualizer _visualizer;
 
-	[SerializeField]
-	MoveInputProvider mo;
+    [SerializeField]
+    GetKeypoints gk;
 
-	private Vector3[] left, right;
+    [SerializeField]
+    MoveInputProvider mo;
 
-	private float leftScore, rightScore;
-	private GetKeypoints.Keypoint[] pose;
-	private bool isRunning = false;
+    private Vector3[] left, right;
 
-	[SerializeField]
-	RawImage image;
+    private float leftScore, rightScore;
+    private GetKeypoints.Keypoint[] pose;
+    private bool isRunning = false;
 
-	[SerializeField]
-	WebCamInput webCamInput;
+    [SerializeField]
+    RawImage image;
 
-	[SerializeField]
-	Text textObject;
+    [SerializeField]
+    WebCamInput webCamInput;
 
-	private string loadText = "";
-	private string[] splitText;
+    [SerializeField]
+    Text message;
 
-	private int textIndex = 0;
-	// Start is called before the first frame update
-	void Start()
-	{
-		pose = gk.pose;
-		state = Phase.PoseEstimation;
-		loadText = (Resources.Load("CalibrationMessage", typeof(TextAsset)) as TextAsset).text;
-		splitText = loadText.Split(char.Parse("\n"));
-	}
+    private string loadText = "";
+    private string[] splitText;
 
-	// Update is called once per frame
-	void Update()
-	{
-		pose = gk.pose;
-		left = _visualizer.GetLeftVert();
-		right = _visualizer.GetRightVert();
+    private int textIndex = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pose = gk.pose;
+        state = Phase.PoseEstimation;
+        loadText = (Resources.Load("CalibrationMessage", typeof(TextAsset)) as TextAsset).text;
+        splitText = loadText.Split(char.Parse("\n"));
+    }
 
-		image.texture = webCamInput.inputImageTexture;
+    // Update is called once per frame
+    void Update()
+    {
+        pose = gk.pose;
+        left = _visualizer.GetLeftVert();
+        right = _visualizer.GetRightVert();
 
-		leftScore = gk.GetScore(GetKeypoints.LeftOrRight.left);
-		rightScore = gk.GetScore(GetKeypoints.LeftOrRight.right);
+        image.texture = webCamInput.inputImageTexture;
 
-		if (pose[0] != null)
-		{
-			switch (state)
-			{
-				case Phase.PoseEstimation:
-					Debug.Log("Phase1");
-					if (pose[0].score > minConfidence && pose[15].score > minConfidence && pose[16].score > minConfidence)
-					{
-						state = Phase.HandEstimation;
-					}
-					return;
+        leftScore = gk.GetScore(GetKeypoints.LeftOrRight.left);
+        rightScore = gk.GetScore(GetKeypoints.LeftOrRight.right);
 
-				case Phase.HandEstimation:
-					Debug.Log("Phase2");
-					if (!isRunning && leftScore > 0.7f && rightScore > 0.7f)
-					{
-						isRunning = true;
-						StartCoroutine(nameof(DecideCloseThreshold));
-					}
-					return;
+        if (pose[0] != null)
+        {
+            switch (state)
+            {
+                case Phase.PoseEstimation:
+                    Debug.Log("Phase1");
+                    message.text = splitText[0];
+                    if (pose[0].score > minConfidence && pose[15].score > minConfidence && pose[16].score > minConfidence)
+                    {
+                        state = Phase.HandEstimation;
+                    }
+                    return;
 
-				case Phase.Positioning:
-					Debug.Log("Phase3");
-					if (!isRunning)
-					{
-						isRunning = true;
-						StartCoroutine(nameof(DecideWalkThreshold));
-					}
-					return;
+                case Phase.HandEstimation:
+                    Debug.Log("Phase2");
+                    if (!isRunning && leftScore > 0.7f && rightScore > 0.7f)
+                    {
+                        isRunning = true;
+                        StartCoroutine(nameof(DecideCloseThreshold));
+                    }
+                    return;
 
-				default:
-					Debug.Log("Calibration end");
-					return;
-			}
-		}
-	}
+                case Phase.Positioning:
+                    Debug.Log("Phase3");
+                    if (!isRunning)
+                    {
+                        isRunning = true;
+                        StartCoroutine(nameof(DecideWalkThreshold));
+                    }
+                    return;
 
-	IEnumerator DecideCloseThreshold()
-	{
-		yield return new WaitForSeconds(5);
-		float[] buffer = new float[60];
-		float delta = 99;
-		float result = 0;
-		for (int i = 0; i < 30; i++)
-		{
-			delta = gk.GetDistance();
-			buffer[i] = delta;
-			yield return null;
-		}
-		result = buffer.Average();
-		result = result - result * 0.1f;
-		gk.closeThreshold = result;
-		PlayerPrefs.SetFloat("CloseThreshold", result);
-		Debug.Log("CloseThreshold is determined");
-		isRunning = false;
-		state = Phase.Positioning;
+                default:
+                    Debug.Log("Calibration end");
+                    return;
+            }
+        }
 
-	}
+        if (end) SceneManager.LoadScene("MainMenu");
+    }
 
-	IEnumerator DecideWalkThreshold()
-	{
-		yield return new WaitForSeconds(5);
-		float[] buffer = new float[200];
-		float delta = 99;
-		float result = 0;
-		for (int i = 0; i < 150; i++)
-		{
-			delta = mo.GetDelta();
-			buffer[i] = delta;
-			yield return null;
-		}
-		result = buffer.Average();
-		result = result - result * 0.1f;
-		mo.forwardThreshold = result;
-		PlayerPrefs.SetFloat("WalkThreshold", result);
-		Debug.Log("WalkThreshold is determined");
-		isRunning = false;
-		state = Phase.End;
-	}
+    IEnumerator DecideCloseThreshold()
+    {
+        yield return new WaitForSeconds(5);
+        float[] buffer = new float[60];
+        float delta, result;
+        for (int i = 0; i < 30; i++)
+        {
+            delta = gk.GetDistance();
+            buffer[i] = delta;
+            yield return null;
+        }
+        result = buffer.Average();
+        result -= result * 0.1f;
+        gk.closeThreshold = result;
+        PlayerPrefs.SetFloat("CloseThreshold", result);
+        Debug.Log("CloseThreshold is determined");
+        isRunning = false;
+        state = Phase.Positioning;
 
-	IEnumerator DecideRotateThreshold()
-	{
-		float[] buffer = new float[60];
-		float delta = 99;
-		float result = 0;
-		for (int i = 0; i < 30; i++)
-		{
-			delta = mo.GetDelta();
-			yield return null;
-		}
-	}
+    }
 
-	IEnumerator Decide() { yield return null; }
+    IEnumerator DecideWalkThreshold()
+    {
+        yield return new WaitForSeconds(5);
+        float[] buffer = new float[200];
+        float delta, result;
+        for (int i = 0; i < 150; i++)
+        {
+            delta = mo.GetDelta();
+            buffer[i] = delta;
+            yield return null;
+        }
+        result = buffer.Average();
+        result -= result * 0.1f;
+        mo.forwardThreshold = result;
+        PlayerPrefs.SetFloat("WalkThreshold", result);
+        Debug.Log("WalkThreshold is determined");
+        isRunning = false;
+        state = Phase.End;
+    }
 
-	void OnDestroy()
-	{
-		PlayerPrefs.Save();
-	}
+    IEnumerator DecideRotateThreshold()
+    {
+        float[] buffer = new float[60];
+        float delta, result;
+        for (int i = 0; i < 30; i++)
+        {
+            delta = mo.GetDelta();
+            yield return null;
+        }
+    }
+
+    IEnumerator Decide() { yield return null; }
+
+    void OnDestroy()
+    {
+        PlayerPrefs.Save();
+    }
 }
