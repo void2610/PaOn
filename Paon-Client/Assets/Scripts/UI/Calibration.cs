e582978742215d6800b5f8f69b3f1c2ddd163f82
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-05 20:37:57 +0900

    Update Calibration.cs

--- Paon-Client/Assets/Scripts/UI/Calibration.cs
+++ Paon-Client/Assets/Scripts/UI/Calibration.cs
@@ -131,7 +131,7 @@ public class Calibration : MonoBehaviour
 	IEnumerator DecideWalkThreshold()
 	{
 		yield return new WaitForSeconds(5);
-		float[] buffer = new float[60];
+		float[] buffer = new float[200];
 		float delta = 99;
 		float result = 0;
 		for (int i = 0; i < 150; i++)
