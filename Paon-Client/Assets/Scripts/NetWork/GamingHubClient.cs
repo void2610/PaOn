b7a0bc36b5ada4ebd2be2be93d65405048facbe7
Harumaso500 <haru006@hotmail.com>
2022-10-05 18:25:54 +0900

    a

--- Paon-Client/Assets/Scripts/NetWork/GamingHubClient.cs
+++ Paon-Client/Assets/Scripts/NetWork/GamingHubClient.cs
@@ -70,7 +70,7 @@ namespace Paon.NNetwork
             {
                 if (player.Name != playerName)
                 {
-                    (this as IGamingHubReceiver).OnJoin(player,Red,Green,Blue);
+                    (this as IGamingHubReceiver).OnJoin(player);
                 }
             }
 
