d68fbd86c608ef5d9390c33acefe2ebc5e06537d
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-05 20:17:10 +0900

    Update MoveInputProvider.cs

--- Paon-Client/Assets/Scripts/Input/MoveInputProvider.cs
+++ Paon-Client/Assets/Scripts/Input/MoveInputProvider.cs
@@ -79,7 +79,7 @@ namespace Paon.NInput
 		void Start()
 		{
 			gk = GK.GetComponent<GetKeypoints>();
-			if (PlayerPrefs.HasKey("WalkThreshold")) forwardThreshold = PlayerPrefs.GetKey("WalkThreshold");
+			if (PlayerPrefs.HasKey("WalkThreshold")) forwardThreshold = PlayerPrefs.GetFloat("WalkThreshold");
 		}
 
 		void Update()
