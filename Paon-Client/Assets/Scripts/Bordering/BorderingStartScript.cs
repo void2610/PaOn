968900e4fc00e7e7eb02aabedd019b027a693c88
void2610 <izumisan2610@gmail.com>
2022-10-07 05:26:30 +0900

    a

--- Paon-Client/Assets/Scripts/Bordering/BorderingStartScript.cs
+++ Paon-Client/Assets/Scripts/Bordering/BorderingStartScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Paon.NPlayer;
 using UnityEngine;
 
 namespace Paon.NBordering
@@ -8,12 +9,20 @@ namespace Paon.NBordering
     {
         public bool starting = false;
 
+        private GameObject Camera;
+
+        void Start()
+        {
+            Camera = GameObject.Find("Main Camera");
+        }
+
         void OnTriggerEnter(Collider other)
         {
             if (other.gameObject.tag == "Player")
             {
                 starting = true;
                 Debug.Log("Bordering Started");
+                Camera.GetComponent<PlayerCameraScript>().stat = 0;
             }
         }
 
