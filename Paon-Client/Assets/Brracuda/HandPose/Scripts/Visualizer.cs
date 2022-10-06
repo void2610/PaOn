d443432807ef05d83dc38911026ee19a8080f276
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-06 21:54:22 +0900

    Update Visualizer.cs

--- Paon-Client/Assets/Brracuda/HandPose/Scripts/Visualizer.cs
+++ Paon-Client/Assets/Brracuda/HandPose/Scripts/Visualizer.cs
@@ -47,25 +47,25 @@ public class Visualizer : MonoBehaviour
 	// Pairs of vertex indices of the lines that make up body's topology.
 	// Defined by the figure in https://google.github.io/mediapipe/solutions/pose.
 	readonly List<Vector4> linePair = new List<Vector4> {
-new Vector4(0, 1),new Vector4(1, 2),
-new Vector4(2, 3),new Vector4(3, 7),
-new Vector4(0, 4),new Vector4(4, 5),
-new Vector4(5, 6),new Vector4(6, 8),
-new Vector4(9, 10),new Vector4(11, 12),
-new Vector4(11, 13),new Vector4(13, 15),
-new Vector4(15, 17),new Vector4(17, 19),
-new Vector4(19, 15),new Vector4(15, 21),
-new Vector4(12, 14),new Vector4(14, 16),
-new Vector4(16, 18),new Vector4(18, 20),
-new Vector4(20, 16),new Vector4(16, 22),
-new Vector4(11, 23),new Vector4(12, 24),
-new Vector4(23, 24),new Vector4(23, 25),
-new Vector4(25, 27),new Vector4(27, 29),
-new Vector4(29, 31),new Vector4(31, 27),
-new Vector4(24, 26),new Vector4(26, 28),
-new Vector4(28, 30),new Vector4(30, 32),
-new Vector4(32, 28)
-									};
+					new Vector4(0, 1), new Vector4(1, 2),
+					new Vector4(2, 3), new Vector4(3, 7),
+					new Vector4(0, 4), new Vector4(4, 5),
+					new Vector4(5, 6), new Vector4(6, 8),
+					new Vector4(9, 10), new Vector4(11, 12),
+					new Vector4(11, 13), new Vector4(13, 15),
+					new Vector4(15, 17), new Vector4(17, 19),
+					new Vector4(19, 15), new Vector4(15, 21),
+					new Vector4(12, 14), new Vector4(14, 16),
+					new Vector4(16, 18), new Vector4(18, 20),
+					new Vector4(20, 16), new Vector4(16, 22),
+					new Vector4(11, 23), new Vector4(12, 24),
+					new Vector4(23, 24), new Vector4(23, 25),
+					new Vector4(25, 27), new Vector4(27, 29),
+					new Vector4(29, 31), new Vector4(31, 27),
+					new Vector4(24, 26), new Vector4(26, 28),
+					new Vector4(28, 30), new Vector4(30, 32),
+					new Vector4(32, 28)
+};
 
 	public bool isDebugEnabled = false;
 
