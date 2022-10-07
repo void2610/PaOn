5914c604dc1c28e572b4cd025c2dca96e0c7b1db
void2610 <izumisan2610@gmail.com>
2022-10-07 09:11:56 +0900

    いらんとこ消した

--- Paon-Client/Assets/Scripts/NaturePlay/FixObjectScript.cs
+++ Paon-Client/Assets/Scripts/NaturePlay/FixObjectScript.cs
@@ -7,37 +7,9 @@ namespace Paon.NNaturePlay
 {
     public class FixObjectScript : MonoBehaviour
     {
-        public CanvasData canvasData = new CanvasData();
-
-        public bool saving = false;
-
-        ///<summary>
-        ///クレヨンの線をJsonファイルで保存するメソッド
-        ///</summary>
-        /// <returns>void</returns>
-        public void SaveLine(
-            LineRenderer lr,
-            string name,
-            int num,
-            string lineName
-        )
-        {
-            LineData ld = new LineData();
-            ld.SetColor (lineName);
-            Vector3[] positions = new Vector3[lr.positionCount];
-            lr.GetPositions (positions);
-            for (int i = 0; i < positions.Length; i++)
-            {
-                positions[i] =
-                    this.gameObject.transform.position - positions[i];
-            }
-            ld.AddLine (positions);
-            ld.Save("Line" + num + "_" + name);
-        }
-
         void OnTriggerStay(Collider other)
         {
-            if (other.CompareTag("HoldableTag"))
+            if (other.gameObject.tag == "HoldableTag")
             {
                 other.gameObject.GetComponent<Rigidbody>().constraints =
                     RigidbodyConstraints.FreezeAll;
@@ -46,65 +18,6 @@ namespace Paon.NNaturePlay
                     new Vector3(90,
                         other.gameObject.transform.eulerAngles.y,
                         -90);
-            }
-
-            if (saving)
-            {
-                if (other.gameObject.name != "PlayerBody")
-                {
-                    if (
-                        other.CompareTag("HoldableTag") ||
-                        other.CompareTag("LineObjectTag")
-                    )
-                    {
-                        Debug.Log("try to save " + other.gameObject.name);
-                        canvasData
-                            .AddObject(other.gameObject,
-                            new Vector3(this.gameObject.transform.position.x -
-                                other.gameObject.transform.position.x,
-                                this.gameObject.transform.position.y -
-                                other.gameObject.transform.position.y,
-                                this.gameObject.transform.position.z -
-                                other.gameObject.transform.position.z));
-                        GameObject.Destroy(other.gameObject);
-                    }
-                    else
-                    {
-                        DateTime dt = DateTime.Now;
-                        string name =
-                            dt.Year.ToString() +
-                            dt.Month.ToString() +
-                            dt.Day.ToString() +
-                            dt.Hour.ToString() +
-                            dt.Minute.ToString() +
-                            dt.Second.ToString();
-
-                        //線を保存
-                        GameObject[] lines =
-                            GameObject.FindGameObjectsWithTag("LineObjectTag");
-                        int num = 0;
-                        foreach (GameObject line in lines)
-                        {
-                            SaveLine(line.GetComponent<LineRenderer>(),
-                            name,
-                            num,
-                            line.name);
-                            Debug.Log("try to save " + line.name);
-                            num++;
-                        }
-
-                        //オブジェクトを保存
-                        canvasData.Save("Canvas_" + name);
-                        canvasData = new CanvasData();
-                        saving = false;
-                        Debug.Log("save successfull");
-                    }
-                }
-                else
-                {
-                    Debug.Log("cant save Player");
-                    saving = false;
-                }
             }
         }
     }
