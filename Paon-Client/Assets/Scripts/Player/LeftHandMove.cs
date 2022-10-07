64a0eade09850f6093889a3ced31498e13659369
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-07 16:32:36 +0900

    tempush

--- Paon-Client/Assets/Scripts/Player/LeftHandMove.cs
+++ Paon-Client/Assets/Scripts/Player/LeftHandMove.cs
@@ -60,7 +60,7 @@ namespace Paon.NPlayer
 
 				if (isCalib)
 				{
-					hand.transform.localPosition = new Vector3(-coords.x / 70 - 4, -coords.y / 70 + 1, hand.transform.localPosition.z);
+					hand.transform.localPosition = new Vector3(-coords.x / 70, -coords.y / 70 + 1, hand.transform.localPosition.z);
 				}
 				else
 					hand.transform.localPosition = new Vector3(-coords.x / 40 + 3, -coords.y / 30 + 1, hand.transform.localPosition.z);
