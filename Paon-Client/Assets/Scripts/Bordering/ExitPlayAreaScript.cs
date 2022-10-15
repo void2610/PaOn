bf652184fe9af932015e411d165cbe1b95e87d7e
void2610 <izumisan2610@gmail.com>
2022-10-15 21:59:40 +0900

    ボルダリング出たらタイマー止まる

--- Paon-Client/Assets/Scripts/Bordering/ExitPlayAreaScript.cs
+++ Paon-Client/Assets/Scripts/Bordering/ExitPlayAreaScript.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
-using Paon.NPlayer;
 using Paon.NNetwork;
+using Paon.NPlayer;
 using UnityEngine;
 
 namespace Paon.NBordering
@@ -12,10 +12,13 @@ namespace Paon.NBordering
 
         private GameObject client;
 
+        private GameObject BM;
+
         void Start()
         {
             NextPosition = GameObject.Find("SpawnPositionAnchor");
             client = GameObject.Find("GameClient");
+            BM = GameObject.Find("BorderingManager");
         }
 
         void OnTriggerStay(Collider other)
@@ -30,6 +33,8 @@ namespace Paon.NBordering
                 other.gameObject.transform.position =
                     NextPosition.transform.position;
                 client.GetComponent<BorderingClient>().OutBorder();
+                BM.GetComponent<BorderingTimerScript>().Timer.CountStop();
+                BM.GetComponent<BorderingTimerScript>().Timer.CountReset();
             }
         }
     }
