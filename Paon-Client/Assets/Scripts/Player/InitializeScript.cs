d9e5f5a51f8f9aefdbec41156688817c2d1a5206
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-15 12:25:38 +0900

    Update InitializeScript.cs

--- Paon-Client/Assets/Scripts/Player/InitializeScript.cs
+++ Paon-Client/Assets/Scripts/Player/InitializeScript.cs
@@ -4,23 +4,30 @@ using UnityEngine;
 
 namespace Paon.NPlayer
 {
-    public class InitializeScript : MonoBehaviour
-    {
-        void Awake()
-        {
-            AudioListener.volume = 0;
-        }
+	public class InitializeScript : MonoBehaviour
+	{
+		void Awake()
+		{
+			AudioListener.volume = 0;
+			StartCoroutine(nameof(DelayAudio));
+		}
 
-        void Update()
-        {
-            if (Time.time < 5)
-            {
-                AudioListener.volume = 0;
-            }
-            else
-            {
-                AudioListener.volume = PlayerPrefs.GetFloat("Volume", 0.5f);
-            }
-        }
-    }
+		// void Update()
+		// {
+		// 	if (Time.time < 5)
+		// 	{
+		// 		AudioListener.volume = 0;
+		// 	}
+		// 	else
+		// 	{
+		// 		AudioListener.volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+		// 	}
+		// }
+
+		IEnumerator DelayAudio()
+		{
+			yield return new WaitForSeconds(5);
+			AudioListener.volume = PlayerPrefs.GetFloat("Volume", 0.5f);
+		}
+	}
 }
