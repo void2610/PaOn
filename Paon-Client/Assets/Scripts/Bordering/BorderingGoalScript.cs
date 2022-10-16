04f0550d05ac909b2f9dd3c5271a4cc52141ee38
LITl-l <52400018+LITl-l@users.noreply.github.com>
2022-10-16 10:23:21 +0900

    Update BorderingGoalScript.cs

--- Paon-Client/Assets/Scripts/Bordering/BorderingGoalScript.cs
+++ Paon-Client/Assets/Scripts/Bordering/BorderingGoalScript.cs
@@ -6,86 +6,88 @@ using UnityEngine.UI;
 
 namespace Paon.NBordering
 {
-    public class BorderingGoalScript : MonoBehaviour
-    {
-        public AudioClip SE;
+	public class BorderingGoalScript : MonoBehaviour
+	{
+		public AudioClip SE;
 
-        public bool goaling = false;
+		public bool goaling = false;
 
-        private GameObject GoalPosition;
+		private GameObject GoalPosition;
 
-        private GameObject GoalText;
+		private GameObject GoalText;
 
-        private GameObject BorderingManager;
+		private GameObject BorderingManager;
 
-        private GameObject Player;
+		private GameObject Player;
 
-        private GameObject client;
+		private GameObject client;
 
-        private bool tmp = false;
+		private bool tmp = false;
 
-        private float goalTime = -30.0f;
+		private float goalTime = -30.0f;
 
-        private float goalCooldown = 30.0f;
+		private float goalCooldown = 30.0f;
 
-        void Start()
-        {
-            BorderingManager = GameObject.Find("BorderingManager");
-            Player = GameObject.Find("PlayerBody");
-            GoalPosition = GameObject.Find("GoalAnchor");
-            GoalText = GameObject.Find("GoalText");
-            client = GameObject.Find("GamingHubClient");
-        }
+		void Start()
+		{
+			BorderingManager = GameObject.Find("BorderingManager");
+			Player = GameObject.Find("PlayerBody");
+			GoalPosition = GameObject.Find("GoalAnchor");
+			GoalText = GameObject.Find("GoalText");
+			client = GameObject.Find("GamingHubClient");
+		}
 
-        void Update()
-        {
-            Debug.Log("Goaled : " + goalTime);
-            if (!tmp && goaling && (Time.time - goalTime > goalCooldown))
-            {
-                goalTime = Time.time;
-                if (this.GetComponent<AudioSource>())
-                {
-                    this.GetComponent<AudioSource>().PlayOneShot(SE);
-                }
+		void Update()
+		{
+			Debug.Log("Goaled : " + goalTime);
+			if (!tmp && goaling && (Time.time - goalTime > goalCooldown))
+			{
+				goalTime = Time.time;
+				if (this.GetComponent<AudioSource>())
+				{
+					this.GetComponent<AudioSource>().PlayOneShot(SE);
+				}
 
-                this
-                    .BorderingManager
-                    .GetComponent<BorderingTimerScript>()
-                    .Timer
-                    .CountEnd();
-                Player.transform.position = GoalPosition.transform.position;
-                Player.GetComponent<Rigidbody>().useGravity = true;
-                client
-                    .GetComponent<GamingHubClient>()
-                    .TimeAsync(PlayerPrefs.GetString("Name", "NULLTYAN"),
-                    goalTime);
-            }
-            if (Time.time - goalTime < 5)
-            {
-                GoalText.GetComponent<Text>().text = "ゴール！！";
-            }
-            else
-            {
-                GoalText.GetComponent<Text>().text = "";
-            }
-            tmp = goaling;
-        }
+				this
+						.BorderingManager
+						.GetComponent<BorderingTimerScript>()
+						.Timer
+						.CountEnd();
+				Player.transform.position = GoalPosition.transform.position;
+				Player.GetComponent<Rigidbody>().useGravity = true;
+				client
+						.GetComponent<GamingHubClient>()
+						.TimeAsync(PlayerPrefs.GetString("Name", "NULLTYAN"),
+						goalTime);
+			}
+			if (Time.time - goalTime < 5)
+			{
+				GoalText.GetComponent<Text>().text = "ゴール！！";
+			}
+			else
+			{
+				GoalText.GetComponent<Text>().text = "";
+			}
+			tmp = goaling;
+		}
 
-        void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.tag == "Player")
-            {
-                goaling = true;
-            }
-            else
-            {
-                goaling = false;
-            }
-        }
+		void OnTriggerEnter(Collider other)
+		{
+			if (other.gameObject.tag == "Player")
+			{
+				goaling = true;
+				Player.GetComponent<Rigidbody>().useGravity = true;
 
-        void OnTriggerExit(Collider other)
-        {
-            goaling = false;
-        }
-    }
+			}
+			else
+			{
+				goaling = false;
+			}
+		}
+
+		void OnTriggerExit(Collider other)
+		{
+			goaling = false;
+		}
+	}
 }
