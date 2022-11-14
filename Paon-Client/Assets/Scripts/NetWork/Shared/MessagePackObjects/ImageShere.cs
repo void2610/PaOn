1a7e14e45dbac68e2de20a5f4e141cf138a729ca
Harumaso500 <haru006@hotmail.com>
2022-11-14 17:55:16 +0900

    a

---
+++ Paon-Client/Assets/Scripts/NetWork/Shared/MessagePackObjects/ImageShere.cs
@@ -0,0 +1,18 @@
+﻿using MessagePack;
+
+
+namespace Paon.NNetwork.Shared.MessagePackObjects
+{
+    [MessagePackObject]
+    public class ImageShere
+    {
+        [Key(0)]
+        public string SenderUUID { get; set; }
+
+        [Key(1)]
+        public byte[] EncodedData { get; set; }
+
+        [Key(2)]
+        public int DataLength { get; set; }
+    }
+}
