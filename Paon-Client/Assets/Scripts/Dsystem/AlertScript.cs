7fdcc06f64fac1aca20b24098d76be167986f3c5
void2610 <izumisan2610@gmail.com>
2022-12-15 01:21:35 +0900

    とりあえず警告

--- Paon-Client/Assets/Scripts/Dsystem/AlertScript.cs
+++ Paon-Client/Assets/Scripts/Dsystem/AlertScript.cs
@@ -2,26 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+
 namespace Dsystem
 {
-  public class AlertScript : MonoBehaviour
-  {
-
-    private Image img;
-    public bool isAlerted = false;
-    void Start()
+    public class AlertScript : MonoBehaviour
     {
-      img = GameObject.Find("RedImage").GetComponent<Image>();
-      img.color = Color.clear;
-    }
+        private Image img;
 
-    // Update is called once per frame
-    void Update()
-    {
-      if (isAlerted)
-      {
-        this.img.color = new Color(0.5f, 0f, 0f, 0.5f);
-      }
+        private Text text;
+
+        public bool isAlerted = false;
+
+        void Start()
+        {
+            text = GameObject.Find("AlertText").GetComponent<Text>();
+            img = GameObject.Find("RedImage").GetComponent<Image>();
+            img.color = Color.clear;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (isAlerted)
+            {
+                text.text = "やめてね";
+                this.img.color = new Color(0.5f, 0f, 0f, 0.4f);
+            }
+            else
+            {
+                text.text = "";
+                this.img.color = new Color(0, 0, 0, 0);
+            }
+        }
     }
-  }
 }
