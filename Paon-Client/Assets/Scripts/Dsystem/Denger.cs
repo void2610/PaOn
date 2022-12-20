a8f632274a47d37fa8379725bbdbb34f3c6c747e
void2610 <izumisan2610@gmail.com>
2022-12-20 17:38:05 +0900

    違反回数保存

--- Paon-Client/Assets/Scripts/Dsystem/Denger.cs
+++ Paon-Client/Assets/Scripts/Dsystem/Denger.cs
@@ -1,37 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Dsystem{
-    public class Denger : MonoBehaviour
+namespace Dsystem
+{
+  public class Denger : MonoBehaviour
+  {
+    public string name = "No Name";
+
+    public string key = "No Key";
+    public string description = "No description";
+
+    private AlertManageScript ams;
+
+    private float sTime;
+    private float coolDown = 10.0f;
+    private bool canActivate = true;
+    public virtual void Start()
     {
-        public string name = "No Name";
-        public string description = "No description";
-
-        private AlertManageScript ams;
-
-        private float sTime;
-        private float coolDown = 10.0f;
-        private bool canActivate = true;
-        public virtual void Start(){
-            ams = GameObject.Find("AlertManager").GetComponent<AlertManageScript>();
-        }
-
-        public virtual void Update(){
-            if(Time.time - sTime > coolDown){
-                canActivate = true;
-            }
-        }
-        public void ShowAlert(){
-            ams.nowName = name;
-            ams.nowDescription = description;
-            ams.isAlerted = true;
-        }
-        public virtual void Activation(){
-            if(canActivate){
-                canActivate = false;
-                sTime = Time.time;
-                ShowAlert();
-            }
-        }
+      ams = GameObject.Find("AlertManager").GetComponent<AlertManageScript>();
     }
+
+    public virtual void Update()
+    {
+      if (Time.time - sTime > coolDown)
+      {
+        canActivate = true;
+      }
+    }
+    public void ShowAlert()
+    {
+      Debug.Log(PlayerPrefs.GetInt(key, 0));
+      ams.nowName = name;
+      ams.nowDescription = description;
+      ams.isAlerted = true;
+    }
+    public virtual void Activation()
+    {
+      if (canActivate)
+      {
+        PlayerPrefs.SetInt(key, PlayerPrefs.GetInt(key, 0) + 1);
+        canActivate = false;
+        sTime = Time.time;
+        ShowAlert();
+      }
+    }
+  }
 }
--- Paon-Client/Assets/Scripts/Dsystem/Denger_Road.cs
+++ Paon-Client/Assets/Scripts/Dsystem/Denger_Road.cs
@@ -1,20 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-namespace Dsystem{
-    public class Denger_Road : Denger
+namespace Dsystem
+{
+  public class Denger_Road : Denger
+  {
+    public override void Start()
     {
-        public override void Start()
-        {
-            base.Start();
-            name = "どうろにでるな";
-            description = "しぬぞ";
-        }
-
-        public override void Update()
-        {
-            base.Update();
-            
-        }
+      base.Start();
+      key = "Denger_Road";
+      name = "どうろにでるな";
+      description = "しぬぞ";
     }
+
+    public override void Update()
+    {
+      base.Update();
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+      if (other.gameObject.name == "PlayerBody")
+      {
+        Activation();
+      }
+    }
+  }
 }
